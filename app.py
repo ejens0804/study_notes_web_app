902ef9c8f9b525b15171f11ca668ed4fd169f861
Ethan Jensen <ejens0804@byui.edu>
2026-02-17 10:27:40 -0700

    moved code in app.py; no major changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.templating import Jinja2Templates  
 from fastapi.staticfiles import StaticFiles
 from datetime import datetime
 import random
